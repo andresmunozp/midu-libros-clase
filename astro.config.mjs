0e590bbb941e3034a5941688b67fc88a1a4b8ef7
andresmunozp <amunozprado@unicauca.edu.co>
2025-07-21 14:22:13 -0500

    libro recomendados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
+    }
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: vercel()
+});
